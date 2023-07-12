c89af747cb1b8a8275359f5c32abc1e553e30d81
Quesaria <133211747+Quesaria@users.noreply.github.com>
2023-07-12 21:14:45 +0600

    foun

--- main.py
+++ main.py
@@ -12,20 +12,18 @@ logger.setLevel(logging.DEBUG)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-   username = message.from_user.username
-   bot.reply_to(message, f'Hi, {username}')
+    username = message.from_user.username
+    bot.reply_to(message, f'Hi, {username}')
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
-   json_string = request.get_data().decode("utf-8")
-   update = telebot.types.Update.de_json(json_string)
-   bot.process_new_updates([update])
-   return "!", 200
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 
 if __name__ == "__main__":
-   bot.remove_webhook()
-   server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
+    bot.remove_webhook()
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
